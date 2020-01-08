51bec7c460c98a16c73633cb76b19ddb45d49d7c
Dominic Yung <dominic_yung@cathaypacific.com>
2020-01-08 15:28:49 +0800

    Change to use secret file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,8 @@
 provider "proxmox" {
   pm_tls_insecure = true
+  pm_api_url      = yamldecode(data.local_file.secrets.content).pm_api_url
+  pm_user         = yamldecode(data.local_file.secrets.content).pm_user
+  pm_password     = yamldecode(data.local_file.secrets.content).pm_password
 }
 
 provider "tls" {
@@ -8,4 +11,8 @@ provider "tls" {
 
 provider "random" {
   version = "~> 2.2"
+}
+
+provider "local" {
+  version = "~> 1.4"
 }
--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,5 +1,9 @@
+data "local_file" "secrets" {
+  filename = "./.terraform_secret.yaml"
+}
+
 data "tls_public_key" "dy2k" {
-  private_key_pem = "${file("./id_rsa")}"
+  private_key_pem = "${yamldecode(data.local_file.secrets.content).id_rsa}"
 }
 
 resource "random_password" "ubuntu_root" {
