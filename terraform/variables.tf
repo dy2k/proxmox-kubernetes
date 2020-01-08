b2ee7dca0bd609eb37498155e8cf835557408af3
Dominic Yung <dy2k@users.noreply.github.com>
2020-01-09 00:52:04 +0800

    Add vm qemu variables and config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,38 +29,53 @@ variable "gateways" {
 }
 
 variable "masters" {
-  type = map(list(map(string)))
+  type = map(map(string))
   default = {
-    kube-master1 = [
-      {
-
-      }
-    ],
-    kube-master2 = [
-      {
-
-      }
-    ]
+    kube-master1 = {
+      cidr    = "10.0.0.11/24"
+      cores   = 2
+      gw      = "10.0.0.1"
+      macaddr = "6E:DE:EE:62:37:1D"
+      ip      = "10.0.0.11"
+      memory  = 2048
+    },
+    kube-master2 = {
+      cidr    = "10.0.0.12/24"
+      cores   = 2
+      gw      = "10.0.0.1"
+      macaddr = "2E:6E:FC:F0:A1:CB"
+      ip      = "10.0.0.12"
+      memory  = 2048
+    }
   }
 }
 
 variable "workers" {
-  type = map(list(map(string)))
+  type = map(map(string))
   default = {
-    kube-worker1 = [
-      {
-
-      }
-    ],
-    kube-worker2 = [
-      {
-
-      }
-    ],
-    kube-worker3 = [
-      {
-
-      }
-    ]
+    kube-worker1 = {
+      cidr    = "10.0.0.21/24"
+      cores   = 4
+      gw      = "10.0.0.1"
+      macaddr = "62:0E:E4:E4:7B:46"
+      ip      = "10.0.0.21"
+      memory  = 4096
+    },
+    kube-worker2 = {
+      cidr    = "10.0.0.22/24"
+      cores   = 4
+      gw      = "10.0.0.1"
+      macaddr = "5A:B1:D9:D1:E6:35"
+      ip      = "10.0.0.22"
+      memory  = 4096
+    },
+    kube-worker3 = {
+      cidr    = "10.0.0.23/24"
+      cores   = 4
+      gw      = "10.0.0.1"
+      macaddr = "22:92:5D:6B:7F:A1"
+      ip      = "10.0.0.23"
+      memory  = 4096
+    },
   }
 }
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,21 +1,69 @@
-resource "proxmox_vm_qemu" "kube-master" {
+resource "proxmox_vm_qemu" "kube-cluster" {
   for_each = var.masters
 
   name        = each.key
   target_node = "proxmox"
+  agent       = 1
+  iso         = "local:iso/ubuntu-18.04.3-live-server-amd64.iso"
+  memory      = each.value.memory
+  cores       = each.value.cores
+  vga {
+    type = "std"
+  }
+  network {
+    id       = 0
+    model    = "virtio"
+    macaddr  = each.value.macaddr
+    bridge   = "vmbr1"
+    firewall = true
+  }
+  disk {
+    id           = 0
+    type         = "scsi"
+    storage      = "local"
+    storage_type = "lvm"
+    size         = "80G"
+    format       = "qcow2"
+  }
+  serial {
+    id   = 0
+    type = "socket"
+  }
+  os_type      = "ubuntu"
+  ipconfig0    = "ip=${each.value.cidr},gw=${each.value.gw}"
+  ciuser       = "root"
+  cipassword   = random_password.ubuntu_root.result
+  searchdomain = "sd-51798.dy2k.io"
+  nameserver   = "10.0.0.1"
+  sshkeys = join("", [
+    data.tls_public_key.dy2k.public_key_openssh,
+    tls_private_key.ubuntu_dyung.public_key_openssh
+  ])
 
   depends_on = [
     proxmox_lxc.gateway
   ]
-}
 
-resource "proxmox_vm_qemu" "kube-worker" {
-  for_each = var.workers
+  connection {
+    host                = each.value.ip
+    private_key         = tls_private_key.ubuntu_dyung.private_key_pem
+    bastion_host        = "ubuntu.dy2k.io"
+    bastion_private_key = data.tls_public_key.dy2k.private_key_pem
+  }
 
-  name        = each.key
-  target_node = "proxmox"
-
-  depends_on = [
-    proxmox_vm_qemu.kube-master
-  ]
+  provisioner "remote-exec" {
+    inline = [
+      "ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N \"\"",
+      "echo \"${tls_private_key.ubuntu_root.private_key_pem}\" > /root/.ssh/id_rsa",
+      "echo \"${tls_private_key.ubuntu_root.public_key_pem}\" > /root/.ssh/id_rsa.pub",
+      "echo \"${tls_private_key.ubuntu_root.public_key_openssh}\" >> /root/.ssh/authorized_keys",
+      "adduser --disabled-password --gecos \"\" dyung && usermod -aG sudo dyung",
+      "su - dyung -c 'ssh-keygen -b 2048 -t rsa -f /home/dyung/.ssh/id_rsa -q -N \"\"'",
+      "su - dyung -c 'echo \"${tls_private_key.ubuntu_dyung.private_key_pem}\" > /home/dyung/.ssh/id_rsa'",
+      "su - dyung -c 'echo \"${tls_private_key.ubuntu_dyung.public_key_pem}\" > /home/dyung/.ssh/id_rsa.pub'",
+      "su - dyung -c 'echo \"${tls_private_key.ubuntu_dyung.public_key_openssh}\" > /home/dyung/.ssh/authorized_keys'",
+      "su - dyung -c 'echo \"${data.tls_public_key.dy2k.public_key_openssh}\" >> /home/dyung/.ssh/authorized_keys'",
+      "chmod 700 /home/dyung/.ssh/authorized_keys"
+    ]
+  }
 }
