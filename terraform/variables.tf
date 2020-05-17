5852937d94e3582bac17394db3252ba6c9aebb85
Dominic Yung <dy2k@users.noreply.github.com>
2020-05-17 11:59:37 +0800

    Change to three masters and align eth0 to private

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,14 +4,14 @@ variable "gateways" {
     gateway1 = [
       {
         cidr   = "10.0.0.1/24"
-        name   = "eth1"
+        name   = "eth0"
         gw     = ""
         hwaddr = "06:08:72:DD:89:B4"
         ip     = "10.0.0.1"
       },
       {
         cidr   = "163.172.118.179/32"
-        name   = "eth0"
+        name   = "eth1"
         gw     = "62.210.0.1"
         hwaddr = "52:54:00:00:bc:0b"
       }
@@ -37,8 +37,8 @@ variable "masters" {
       gw      = "10.0.0.1"
       macaddr = "6E:DE:EE:62:37:1D"
       ip      = "10.0.0.11"
-      memory  = 3072
-      disk    = "80G"
+      memory  = 2048
+      disk    = "40G"
     },
     kube-master2 = {
       cidr    = "10.0.0.12/24"
@@ -46,8 +46,17 @@ variable "masters" {
       gw      = "10.0.0.1"
       macaddr = "2E:6E:FC:F0:A1:CB"
       ip      = "10.0.0.12"
-      memory  = 3072
-      disk    = "80G"
+      memory  = 2048
+      disk    = "40G"
+    },
+    kube-master3 = {
+      cidr    = "10.0.0.13/24"
+      cores   = 2
+      gw      = "10.0.0.1"
+      macaddr = "6A:83:72:97:97:81"
+      ip      = "10.0.0.13"
+      memory  = 2048
+      disk    = "40G"
     }
   }
 }
@@ -57,7 +66,7 @@ variable "workers" {
   default = {
     kube-worker1 = {
       cidr    = "10.0.0.21/24"
-      cores   = 4
+      cores   = 2
       gw      = "10.0.0.1"
       macaddr = "62:0E:E4:E4:7B:46"
       ip      = "10.0.0.21"
@@ -66,7 +75,7 @@ variable "workers" {
     },
     kube-worker2 = {
       cidr    = "10.0.0.22/24"
-      cores   = 4
+      cores   = 2
       gw      = "10.0.0.1"
       macaddr = "5A:B1:D9:D1:E6:35"
       ip      = "10.0.0.22"
@@ -75,7 +84,7 @@ variable "workers" {
     },
     kube-worker3 = {
       cidr    = "10.0.0.23/24"
-      cores   = 4
+      cores   = 2
       gw      = "10.0.0.1"
       macaddr = "22:92:5D:6B:7F:A1"
       ip      = "10.0.0.23"
