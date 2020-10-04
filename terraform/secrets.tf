b3b830bb0ab11918d75dfec43548004cacb644d2
Dominic Yung <dy2k@users.noreply.github.com>
2020-10-04 13:15:58 +0800

    Rename tls key name

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -3,9 +3,9 @@ data "local_file" "secrets" {
 }
 
 data "tls_public_key" "dy2k" {
-  private_key_pem = yamldecode(data.local_file.secrets.content).id_rsa
+  private_key_pem = yamldecode(data.local_file.secrets.content).ssh_key
 }
 
 data "tls_public_key" "ubuntu_terraform" {
-  private_key_pem = yamldecode(data.local_file.secrets.content).terraform
+  private_key_pem = yamldecode(data.local_file.secrets.content).terraform_key
 }
